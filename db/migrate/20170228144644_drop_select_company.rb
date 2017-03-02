ab357be577e64845e9e647df90701f2bf27077cc
Udit Jindal <jindaludit23@gmail.com>
2017-03-02 18:57:16 +0530

    Update 20170228144644_drop_select_company.rb

--- db/migrate/20170228144644_drop_select_company.rb
+++ db/migrate/20170228144644_drop_select_company.rb
@@ -1,9 +1,5 @@
 class DropSelectCompany < ActiveRecord::Migration
   def change
-<<<<<<< HEAD
-=======
-  	#drop_table :select_companies
->>>>>>> 8d04d25250511110d1f497d7522d2f5a7b055e7f
   	drop_table :users
   end
 
